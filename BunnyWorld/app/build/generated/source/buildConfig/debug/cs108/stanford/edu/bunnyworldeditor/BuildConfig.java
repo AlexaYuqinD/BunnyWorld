3ec2232f38eb43305d514c4c00dd908ef191223a
Yuqin Dai <dyq4430@126.com>
2020-03-15 21:07:22 -0700

    add file

---
+++ BunnyWorld/app/build/generated/source/buildConfig/debug/cs108/stanford/edu/bunnyworldeditor/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cs108.stanford.edu.bunnyworldeditor;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cs108.stanford.edu.bunnyworldeditor";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
